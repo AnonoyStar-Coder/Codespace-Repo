a63ae49658574bf72540cf45bd874c3dc99d5e60
AnonoyStar-Coder <151065705+AnonoyStar-Coder@users.noreply.github.com>
2024-03-16 13:28:29 +0000

    yoo

--- Java/Parameterized_Constructor.java
+++ Java/Parameterized_Constructor.java
@@ -2,29 +2,24 @@ class Person {
     String name;
     int age;
 
-    // Parameterized constructor
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Method to display person information
     public void displayInfo() {
         System.out.println("Name: " + name);
         System.out.println("Age: " + age);
     }
 }
 
-public class Main {
+public class Parameterized_Constructor {
     public static void main(String[] args) {
-        // Creating a Person object using the parameterized constructor
         Person person1 = new Person("John", 30);
         Person person2 = new Person("Alice", 25);
 
-        // Displaying information for person1
         person1.displayInfo();
 
-        // Displaying information for person2
         person2.displayInfo();
     }
 }
