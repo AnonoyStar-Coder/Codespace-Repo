e13c734dbd091683cab2e869a21b884f25f724bb
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-02-05 22:52:35 +0530

    new additions
    Deciding to redo the system, hence new files.

---
+++ Python/test.py
@@ -0,0 +1,11 @@
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Calculate the sum and product
+sum = num1 + num2
+product = num1 * num2
+
+# Display the results
+print("Sum:", sum)
+print("Product:", product) 
