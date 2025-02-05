c1c0e222529af6cac06e7fc9e2007caf3d0df541
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-02-05 23:04:01 +0530

    new

---
+++ Extras/typecraft.sh
@@ -0,0 +1,4 @@
+sudo pacman -Syyu
+sudo pacman -S git nvim zsh tmux zsh-completions zsh-syntax-highlighting zsh-autosuggestions fzf ripgrep bat eza fd lazygit docker docker-compose docker-zsh-completion lazydocker
+git clone https://github.com/typecraft-dev/dotfiles.git
+cd dotfiles
