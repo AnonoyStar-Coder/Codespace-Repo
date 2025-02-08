087f10a18046db0b7631602550347650ffdc0823
AnonoyStar-Coder <anonystargamerz@gmail.com>
2025-02-09 04:58:43 +0530

    moved some files

--- Install_Scripts/test.sh
+++
@@ -1,9 +0,0 @@
-# /bin/bash
-docker run -w / -it ubuntu sh -uec '
-  apt-get update -y
-  apt-get install -y wget tar vim less
-  wget https://github.com/sayanarijit/xplr/releases/latest/download/xplr-linux.tar.gz
-  tar -xzvf xplr-linux.tar.gz
-  ./xplr
-'
-
